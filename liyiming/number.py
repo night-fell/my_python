9c17c468a495c745971495d7bdfd58c391109ae6
liqing <liqing@wecash.net>
2017-07-11 07:57:53 +0800

    20170711_review

--- liyiming/number.py
+++ liyiming/number.py
@@ -10,12 +10,12 @@ if __name__ == "__main__":
     y = 10
 
     # 将x转换为整数
-    print(int(x))    # 将y转换为浮点数
-
-    print(float(y))    # 将x转换为复数， 实数部分为x，虚数部分为0
-
-    print(complex(x))    # 将x，y转换为复数， 实数部分为x，虚数部分为y
-
+    print(int(x))
+    # 将y转换为浮点数
+    print(float(y))
+    # 将x转换为复数， 实数部分为x，虚数部分为0
+    print(complex(x))
+    # 将x，y转换为复数， 实数部分为x，虚数部分为y
     print(complex(x, y))
 
     print(u"常用数学函数")
